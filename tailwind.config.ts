daf75a440d13c9d7aeefe7b5a594da8040bfb8ac
Suchismita Bose <133328192+psychomita@users.noreply.github.com>
2024-09-28 20:35:06 +0530

    Add @tailwindcss/forms plugin to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,8 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
 export default config;
