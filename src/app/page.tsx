3e68696139215783c1a90a235806b08d4c64aa8f
Suchismita Bose <133328192+psychomita@users.noreply.github.com>
2024-09-28 23:01:42 +0530

    Refactor readTodos function to filter todos by completion status and order them by updatedAt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import AddTodo from "@/components/add-todo";
 import Todos, { TodoProps } from "@/components/todo";
-import { readTodos } from '@/lib/actions/todo-actions';
+import { readTodos } from "@/lib/actions/todo-actions";
 
 export default async function Home() {
-  const todosDetails = await readTodos() as TodoProps[];
+  const todosDetails = (await readTodos(false)) as TodoProps[];
+  const completedTodos = (await readTodos(true)) as TodoProps[];
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-     keeptrack
-     <Todos todos={todosDetails}/> 
-     <AddTodo/>
+      keeptrack
+      <div className="flex flex-col space-y-4">
+        <Todos todos={todosDetails} />
+        <Todos todos={completedTodos} />
+      </div>
+      <AddTodo />
     </main>
   );
 }
