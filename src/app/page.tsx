7cf519c4640b8c9e093dc42376c04cc402e73721
Suchismita Bose <133328192+psychomita@users.noreply.github.com>
2024-10-01 16:00:12 +0530

    Refactor page component to include logo, current date, and responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 import AddTodo from "@/components/add-todo";
+import Logo from "@/components/logo";
 import Todos, { TodoProps } from "@/components/todo";
 import { readTodos } from "@/lib/actions/todo-actions";
+import { formatDateLong } from "@/lib/utils";
 
 export default async function Home() {
   const todosDetails = (await readTodos(false)) as TodoProps[];
   const completedTodos = (await readTodos(true)) as TodoProps[];
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      keeptrack
-      <div className="flex flex-col space-y-4">
+      <div className="flex justify-between items-center w-full max-w-lg">
+        <Logo className="w-56 h-auto" />
+        <p className="text-zinc-700 italic">{formatDateLong(new Date())}</p>
+      </div>
+      <div className="flex flex-col space-y-4 mt-8 w-full max-w-lg">
         <Todos todos={todosDetails} />
         <Todos todos={completedTodos} />
+        <div>
+          <AddTodo />
+        </div>
       </div>
-      <AddTodo />
     </main>
   );
 }
